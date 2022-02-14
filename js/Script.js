9a5a1aa17692886c61f74a67430743a18d08e5e1
osamahabibm <40914381+osamahabibm@users.noreply.github.com>
2022-02-14 04:50:39 -0800

    Add files via upload

--- js/Script.js
+++ js/Script.js
@@ -33,6 +33,15 @@ $(function () {
 
 });
 
+const currentlocation = location.href;
+const menuItem = document.querySelectorAll('a')
+const menulength = menuItem.length;
+for (let i = 0; i < menulength; i++) {
+  if (menuItem[i].href === currentlocation) {
+    menuItem[i].className = "active";
+  }
+}
+
 
 const navMenu = document.getElementById("nav_menu"),
 toggleMenu = document.getElementById("toggle-menu"),
